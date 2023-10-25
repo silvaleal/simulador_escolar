67a8d48c6552a14f2d80d27b0fb1497f33780e62
silvaleal <124866471+silvaleal@users.noreply.github.com>
2023-10-25 13:27:08 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Programador: @ojoser1
+
+def menu():
+    print('')
+    print(' Simulador escolar - 2023')
+    print(' 1. Verificar alunos')
+    print(' 2. Verificar professores')
+    print(' 3. Verificar faltas')
+    print(' 4. Modificar registro')
+    print(' 5. Registrar aluno')
+    print(' 5. Registrar professor')
+    print('')
+
+def escolha_menu():
+    menu()
+    escolha = input('Digite o número: ')
+
+while True:
+    escolha_menu()
