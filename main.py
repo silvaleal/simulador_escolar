484b5ddd146d05e14cad24a1bab329cf266d5985
silvaleal <124866471+silvaleal@users.noreply.github.com>
2023-10-25 21:16:44 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,104 @@
 # Programador: @ojoser1
+import os
+import sqlite3  # pip install db-sqlite3.
+import random  # Não requer instalação.
+import time  # Não requer instalação
+
+# Obs.: No banco de dados, chamei os professores de instrutores, então fique obvío que, professor = instrutor :)
+
 
 def menu():
     print('')
     print(' Simulador escolar - 2023')
     print(' 1. Verificar alunos')
     print(' 2. Verificar professores')
-    print(' 3. Verificar faltas')
-    print(' 4. Modificar registro')
-    print(' 5. Registrar aluno')
-    print(' 6. Registrar professor')
+    print(' 3. Verificar curso')
+    print(' 4. Registrar aluno')
+    print(' 5. Registrar professor')
+    print(' 6. Registrar curso')
     print('')
+
 
 def escolha_menu():
     menu()
     escolha = input('Digite o número: ')
 
     if escolha == '1':
-    	pass
+        pass
     elif escolha == '2':
-    	pass
+        pass
     elif escolha == '3':
-    	pass
+        pass
     elif escolha == '4':
-    	pass
+        pass
     elif escolha == '5':
-    	pass
+        nome = input('Digite o nome COMPLETO do professor. \n')
+        idade = int(input('Digite a idade do professor. \n'))
+        salario = int(input('Digite o salário do professor. \n'))
+        registrar_professor(nome, idade, salario)
+        print('')
+        print(f' Professor "{nome}" foi registrado no sistema.')
+        print('')
+        time.sleep(1) # Estarei usando o delay de 1s, fazendo assim, o menu só voltar a aparecer dps de um 1 segundo que a mensagem acima for enviada.
+        
     elif escolha == '6':
-    	pass
+        nome = input('Digite o nome do curso. \n')
+        professor = input('Digite o nome COMPLETO do professor. \n')
+        registrar_curso(nome, professor)
+        print('')
+        print(f' Curso "{nome}"foi registrado no sistema.')
+        print(f' Responsável: {professor}.')
+        print('')
+        time.sleep(1) # Estarei usando o delay de 1s, fazendo assim, o menu só voltar a aparecer dps de um 1 segundo que a mensagem acima for enviada.
+
+
+def gerar_id():
+    return random.randint(10000, 99999)
+
+
+def registrar_aluno():
+    pass
+
+
+def registrar_professor(nome, idade, salario):
+    try:  # Isto é um tratamento de erro, caso o código dentro do "try" esteja certo, ele vai acontecer normalmente.
+        conex = sqlite3.connect('database.db')
+        cursor = conex.cursor()
+
+        cursor.execute(
+            f'INSERT INTO instrutores (instrutor_id, instrutor_nome, instrutor_idade, instrutor_salario) VALUES ({gerar_id()}, "{nome}", {idade}, {salario})')
+        conex.commit()
+    except Exception as error:  # Caso o código dentro do "try" esteja errado, ele irá entrar aqui. :)
+        print(f'Erro encontrado: {error}')
+    finally:  # Ação que irá acontecer no final, é opção, afinal eu poderia ficar a conexão no "try".
+        cursor.close()
+
+
+def registrar_curso(nome, instrutor):
+    try:  # Isto é um tratamento de erro, caso o código dentro do "try" esteja certo, ele vai acontecer normalmente.
+        conex = sqlite3.connect('database.db')
+        cursor = conex.cursor()
+
+        cursor.execute(
+            f'INSERT INTO cursos (curso_id, curso_nome, curso_instrutor) VALUES ({gerar_id()}, "{nome}", "{instrutor}")')
+        conex.commit()
+    except Exception as error:  # Caso o código dentro do "try" esteja errado, ele irá entrar aqui. :)
+        print(f'Erro encontrado: {error}')
+    finally:  # Ação que irá acontecer no final, é opção, afinal eu poderia ficar a conexão no "try".
+        cursor.close()
+
+
+def verificar_aluno():
+    pass
+
+
+def verificar_professor():
+    pass
+
+
+def verificar_curso():
+    pass
+
 
 while True:
     escolha_menu()
--- readme.md
+++ readme.md
@@ -1,10 +1,13 @@
 # SIMULADOR ESCOLAR | Clube robótica 2023
-- Autor: José Rodolfo (@ojoser) 
+
+- Autor: José Rodolfo (@ojoser) | Autobots
 
 # Changelogs
-25/10/23 - Hello, world.
+
+- 25/10/23 - Hello, world.
 
 # Tecnologias
+
 - SQL - Banco de dados (Irá armazenar os dados escolares). (SQlite3)
 - Analise de dados - Criar um dashboard com interação.
 - Python - Linguagem de programação
